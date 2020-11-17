3bd709d3213bec9a770f78d2a353d1a3b884d158
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-17 15:03:36 -0800

    commit tree 875b8035db460107c23fe2e817656583243fb7ba

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+
+class Anagram
+    attr_accessor :words
+    def initialize(words)
+        @words = words
+    end
+  def match(array)
+        array.select do |word| 
+        word.split('').sort.join == @words.split('').sort.join 
+        end 
+    end
+end 
